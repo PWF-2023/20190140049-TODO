f1d7c60926deb06b67f9254a766efd26c2ca24fc
DamasHimawan <damas261100@gmail.com>
2023-05-19 01:01:43 +0700

    tugas6

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,6 +4,7 @@
             {{ __('User') }}
         </h2>
     </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
@@ -87,6 +88,25 @@
                                 <td class="px-6 py-4">
                                     <div class="flex space-x-3">
                                         {{-- Action Here --}}
+
+                                        @if ($user->is_admin)
+                                        <form action="{{ route('user.removeadmin',$user) }}" method="Post">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit" class="text-blue-600 dark:text-blue-400 whitespace-nowrap">Remove Admin</button>
+                                        </form>
+                                        @else
+                                        <form action="{{ route('user.makeadmin',$user) }}" method="Post">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button type="submit" class="text-blue-600 dark:text-blue-400 whitespace-nowrap">Make Admin</button>
+                                        </form>
+                                        @endif
+                                        <form action="{{ route('user.destroy',$user) }}" method="Post">
+                                            @csrf
+                                            @method('delete')
+                                            <button type="submit" class="text-red-600 dark:text-red-400 whitespace-nowrap">Delete</button>
+                                        </form>
                                     </div>
                                 </td>
                             </tr>
